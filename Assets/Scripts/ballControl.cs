5c80c92f0c413ade2f8882c0f187eb0dcfbd35e1
Nightshadow0911 <whqudgh0911@gmail.com>
2023-09-11 20:23:26 +0900

    충돌구현

--- Assets/Scripts/ballControl.cs
+++ Assets/Scripts/ballControl.cs
@@ -2,48 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class ballControll : MonoBehaviour               //마우스방향
-//{
-//    public Rigidbody2D ballRigidbody;
-//    public GameObject paddle;
-//    public float moveSpeed = 5f;
-//    private bool isStopped;
-
-//    void Start()
-//    {
-//        if (ballRigidbody != null)
-//        {
-//            ballRigidbody.velocity = Vector2.zero;
-//        }
-//        isStopped = true;
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (!isStarted)
-//        {
-//            Transform paddleTransform = paddle.transform;
-//            float paddleXPosition = paddleTransform.position.x;
-//            transform.position = new Vector3(paddleXPosition, transform.position.y, 0);
-//        }
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            isStopped = false;
-//            // 마우스 클릭한 지점의 위치를 가져옵니다.
-//            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-//            targetPosition.z = 0f; // 2D 게임에서는 Z 축을 0으로 설정합니다.
-
-//            // 공을 클릭한 위치로 이동시킵니다.
-//            if (ballRigidbody != null)
-//            {
-//                Vector2 direction = (targetPosition - transform.position).normalized;
-//                ballRigidbody.velocity = direction * moveSpeed;
-//            }
-//        }
-//    }
-//}
-public class ballControll : MonoBehaviour           //랜덤방향
+public class ballControl : MonoBehaviour           //랜덤방향
 {
     public Rigidbody2D ballRigidbody;
     public GameObject paddle;
@@ -54,7 +13,8 @@ public class ballControll : MonoBehaviour           //랜덤방향
     void Start()
     {
         ballRigidbody = GetComponent<Rigidbody2D>();
-        randomDirection = Random.insideUnitCircle.normalized;
+        randomDirection = new Vector2(1, 1).normalized; //공 처음 시작방향
+        //randomDirection = Random.insideUnitCircle.normalized;
         if (ballRigidbody != null)
         {
             ballRigidbody.velocity = Vector2.zero;
@@ -65,12 +25,32 @@ public class ballControll : MonoBehaviour           //랜덤방향
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(randomDirection * moveSpeed * Time.deltaTime);
+
+
+        if (transform.position.x < -9 || transform.position.x > 9) //화면 좌우외곽에 부딪혔을때 공이 튕기게
+        {
+            randomDirection.x *= -1;
+        }
+
+        if (transform.position.y > 5) //화면 위쪽 외곽에 부딪혔을때
+        {
+            randomDirection.y *= -1;
+        }
+
+        if(transform.position.y < -5) //화면 아래쪽으로 떨어졌을때
+        {
+            randomDirection.y *= -1; //임시
+            //공이 아래로 떨어졌을때 공 삭제 및 체력 감소 항목 추가
+        }
+
         if (isStopped)
         {
             Transform paddleTransform = paddle.transform;
             float paddleXPosition = paddleTransform.position.x;
             transform.position = new Vector3(paddleXPosition, transform.position.y, 0);
         }
+
         if (Input.GetMouseButtonDown(0))
         {
             isStopped = false;
@@ -79,6 +59,26 @@ public class ballControll : MonoBehaviour           //랜덤방향
             {
                 ballRigidbody.velocity = randomDirection * moveSpeed;
             }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        HandleCollision(collision);
+    }
+
+    void HandleCollision(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Brick")) //벽돌과 충돌했을때 벽돌을 파괴하고 공의 방향을 반대로
+        {
+            Destroy(collision.gameObject);
+            randomDirection.x *= -1;
+            randomDirection.y *= -1;
+        }
+
+        if (collision.gameObject.CompareTag("Paddle")) //패들과 부딪혔을때 공이 튕겨나가도록
+        {
+            randomDirection.y *= -1;
         }
     }
 }
--- Assets/Scripts/paddleControl.cs
+++ Assets/Scripts/paddleControl.cs
@@ -2,31 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PaddleControl : MonoBehaviour
 {
-    public Transform playerPaddle;
+    public GameObject paddlePrefab; // 패들 프리팹에 대한 참조
+    public float paddleSpeed = 5f; // 패들의 이동 속도
+   public GameObject paddle; // 생성된 패들에 대한 참조
+
     void Start()
     {
-
+        // 패들을 생성하고 초기 위치에 배치합니다.
+        paddle = Instantiate(paddlePrefab, transform.position, Quaternion.identity);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 currentScale = playerPaddle.localScale;
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        float x = mousePos.x;
-
-        float paddleHalfLength = currentScale.x / 2f;    //패들의 길이가 변해도 화면 밖으로 나가지 않음
-        if (x + paddleHalfLength > 8.9f)
+        if (paddle != null) // 패들이 아직 생성되지 않았다면 업데이트하지 않습니다.
         {
-            x = 8.9f - paddleHalfLength;
+            Vector3 currentScale = paddle.transform.localScale; // 패들 객체의 스케일 가져오기
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            float x = mousePos.x;
+
+            float paddleHalfLength = currentScale.x / 2f; // 패들의 길이가 변해도 화면 밖으로 나가지 않음
+            if (x + paddleHalfLength > 8.9f)
+            {
+                x = 8.9f - paddleHalfLength;
+            }
+            if (x - paddleHalfLength < -8.9f)
+            {
+                x = -8.9f + paddleHalfLength;
+            }
+
+            // 패들 객체의 위치를 업데이트합니다.
+            paddle.transform.position = new Vector3(x, paddle.transform.position.y, 0);
         }
-        if (x - paddleHalfLength < -8.9f)
-        {
-            x = -8.9f + paddleHalfLength;
-        }
-        transform.position = new Vector3(x, transform.position.y, 0);
     }
 }
