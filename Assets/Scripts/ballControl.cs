ce10b22579f1db94ed6ae4050aed41e50b5e1c58
Nightshadow0911 <whqudgh0911@gmail.com>
2023-09-12 17:00:26 +0900

    대문자통일

--- Assets/Scripts/ballControl.cs
+++ Assets/Scripts/ballControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
-public class ballControl : MonoBehaviour           //��������
+public class BallControl : MonoBehaviour           //��������
 {
     public Rigidbody2D ballRigidbody;
     public GameObject paddle;
