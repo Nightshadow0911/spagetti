889503dab57135bcf4c994607b596bffcf63c4ab
M1NO001 <130371068+M1NO001@users.noreply.github.com>
2023-09-13 20:19:22 +0900

    아이템 적용

--- Assets/Scripts/ItemBrick.cs
+++ Assets/Scripts/ItemBrick.cs
@@ -77,7 +77,11 @@ public class ItemBrick : MonoBehaviour
     }
     private void OnDestroy()
     {
-        BrickItemEffect selectedEffect = ChooseItemEffect(); // 랜덤 아이템 효과 선택
-        ApplyItemEffect(selectedEffect); // 선택된 효과를 적용
+        float random = Random.Range(0f, 1f);
+        if (random < 0.2f)
+        {
+            BrickItemEffect selectedEffect = ChooseItemEffect(); // 랜덤 아이템 효과 선택
+            ApplyItemEffect(selectedEffect); // 선택된 효과를 적용
+        }      
     }
 }
